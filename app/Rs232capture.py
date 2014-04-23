8c0fe0de974a49439de2354c970affd45a0a2849
Xavier <xavier.barrelet@edu.hefr.ch>
2014-04-23 16:20:46 +0200

    Class rewritten to take in account the changes wanted by M. Caprara. Now everything should be working.
    If the serial connection is not openend, the thread waits for its opening

--- app/Rs232capture.py
+++ app/Rs232capture.py
@@ -7,227 +7,252 @@ import time
 
 
 class RS232captureThread(threading.Thread):
-
     # configure the serial connection
-    global ser
-    ser = serial.Serial(
-        port="/dev/ttyUSB0",
-        baudrate=1200,
-        parity=serial.PARITY_EVEN,
-        stopbits=serial.STOPBITS_ONE,
-        bytesize=serial.EIGHTBITS
-    )
-    global oldPacket
+
+    isConnected = False
+    while not isConnected:
+        try:
+            isConnected = True
+            ser = serial.Serial(
+                port="/dev/ttyUSB0",
+                baudrate=1200,
+                parity=serial.PARITY_EVEN,
+                stopbits=serial.STOPBITS_ONE,
+                bytesize=serial.EIGHTBITS
+            )
+        except:
+            print "Serial connection problem"
+            isConnected = False
+            time.sleep(5)
+
     oldPacket = ""
+    currentNumber = 0
+    scorePacket = ""
+    isRunning = False
 
-    isRunning=False
-    hasDisplayed=False
-
-    global SYNCHRO, BLANK
-    hundredNumberRunner = 0
     SYNCHRO = "ff"
     BLANK = "aa"
-    penaltyTime = 0
 
     ser.flush()
     ser.close()
     ser.open()
-    print "ser is now open"
+
+    if ser.isOpen():
+        app.config["CHRONO_CONNECT"] = True
+        print "the serial connection is now open"
+    else:
+        print "the serial connection is not opened"
 
     ser.setDTR(True)  #confirms the connection
     ser.setRTS(False)
-    print "DTR is set on 1 now"
+    print "DTR is set on true now and RTS on false"
 
-    global testPacketParcours, testPacketClassement
-    testPacketParcours = "aa28000404000000aa1aa2aaaa0000ff"
-    testPacketClassement = "2553000404000000aa1aa1aaaa0000ff"
 
-    def getRankPacket(self,rank):
+    def ishex(self, val):  #from https://coderwall.com/p/bfqwba
+        try:
+            int(val, 16)
+            return True
+        except ValueError, e:
+            return False
+
+    def getRankPacket(self, rank):
         if rank < 10:
             return "bccb0d0" + str(rank) + "0000000000000000000000ff"
         else:
             return "bccb0d" + str(rank) + "0000000000000000000000ff"
 
-    def getPointPacket(self,point): #Not currently used
+    def getPointPacket(self, point):  #Not currently used
         return "000000" "d0" + "0000000000000000000000ff"  #PO at the start
 
 
-    def getOverlappingPacket(self,overlap): #Not currently used
+    def getOverlappingPacket(self, overlap):  #Not currently used
         return "000000" "bd" + "0000000000000000000000ff"  #dE at the start
 
-    def checkPCCommand(self, pcCommand, currNumber):
-
-        currRunner=db.session.query(Participant).filter(Participant.num_depart == currNumber
-                    and Participant.id_epreuve == app.config["CURRENT_EPREUVE_ID"]).first()
-
-        def reset():
+    def checkRunner(self):
+        checkingRunner = db.session.query(Participant).filter(Participant.num_depart == self.currentNumber
+                                                              and Participant.id_epreuve == app.config[
+                                                                  "CURRENT_EPREUVE_ID"])
+        if "True" in str(db.session.query(checkingRunner.exists()).all()):
+            return
+        else:
+            runnerToAdd = Participant(
+                num_depart=self.currentNumber, id_epreuve=app.config["CURRENT_EPREUVE_ID"])
+            db.session.add(runnerToAdd)
+            db.session.commit()
             return
 
+
+    def getRunner(self):
+        return db.session.query(Participant).filter(Participant.num_depart == self.currentNumber
+                                                    and Participant.id_epreuve == app.config[
+                                                        "CURRENT_EPREUVE_ID"]).first()
+
+    def checkPCCommand(self):
+
+        currRunner = self.getRunner()
+
+        def nothing():
+            return
+
+        def reset():
+            if self.oldPacket[0] != self.BLANK[0]:
+                if self.isRunning:
+                    print "Launching saveRunner..."
+                    self.saveRunner()
+                    if app.config["SEND_AFF"]:
+                        self.display()
+
         def eliminated():
-            if currRunner.temps_init == None:
-                currRunner.etat_init="elimine"
-            elif currRunner.temps_barr == None:
-                currRunner.etat_barr="elimine"
+            if currRunner.temps_init == 0:
+                currRunner.etat_init = "elimine"
+            elif currRunner.temps_barr == 0:
+                currRunner.etat_barr = "elimine"
             else:
-                currRunner.etat_barr2="elimine"
+                currRunner.etat_barr2 = "elimine"
             db.session.commit()
 
         def gaveUp():
-            if currRunner.temps_init == None:
-                currRunner.etat_init="abandon"
-            elif currRunner.temps_barr == None:
-                currRunner.etat_barr="abandon"
+            if currRunner.temps_init == 0:
+                currRunner.etat_init = "abandon"
+            elif currRunner.temps_barr == 0:
+                currRunner.etat_barr = "abandon"
             else:
-                currRunner.etat_barr2="abandon"
+                currRunner.etat_barr2 = "abandon"
             db.session.commit()
 
         def HC():
-            currRunner.hc=True
+            currRunner.hc = True
             db.session.commit()
 
-        def incrTime():
-            global penaltyTime
-            penaltyTime += 100  #TODO : Check the bareme here or \
-            # simply add the points and do it when the classement is established?
+        def changeNumber():
+            if self.oldPacket[6:8].isdigit():
+                self.currentNumber = int(self.oldPacket[6:8])
+                self.checkRunner()
+                print "Number is now : ", self.currentNumber
 
         def IncrNumByHundred():
-            self.hundredNumberRunner += 1
+            self.currentNumber += 100
+            print "Increased Number is now : ", self.currentNumber
 
-        options = {'0': reset,
+        def ldbarr():
+            self.saveRunner()
+            return
+
+        options = {'0': nothing,
                    '1': reset,
                    '2': eliminated,
                    '3': gaveUp,
                    '4': HC,
-                   '5': reset,
-                   '6': reset,
-                   '7': reset,
-                   '8': reset,
-                   '9': incrTime,
-                   'a': reset,
+                   '5': nothing,
+                   '6': nothing,
+                   '7': changeNumber,
+                   '8': nothing,
+                   '9': nothing,
+                   'a': nothing,
                    'b': IncrNumByHundred,
-                   'c': reset,
-                   'd': reset,
-                   'e': reset,
+                   'c': nothing,
+                   'd': ldbarr,
+                   'e': nothing,
+                   'f': nothing
         }
-        options[pcCommand]()
+        options[self.oldPacket[4]]()
 
 
+    def display(self):
+        self.ser.setRTS(True)
+        print "RTS set on true. Now displaying for runner number : ", self.currentNumber
 
-    def display(self, currNumber, timePacket):
-        ser.setRTS(True)
-        print "Now displaying for runner number : "+str(currNumber)
-
-        currentCode = db.session.query(Epreuve).filter(Epreuve.id_epreuve == app.config["CURRENT_EPREUVE_ID"])\
+        currentCode = db.session.query(Epreuve).filter(Epreuve.id_epreuve == app.config["CURRENT_EPREUVE_ID"]) \
             .first().bareme_code
 
         res = bareme.Baremes.doBaremes(currentCode)
-        infos = next((item for item in res if item["num_depart"] == currNumber), None) #In case more infos are needed
-        rankPacket=self.getRankPacket(infos["rang"])
-        rankPacket=rankPacket.decode("hex")
+        infos = next((item for item in res if item["num_depart"] == self.currentNumber),
+                     None)  #In case more infos are needed
+        rankPacket = self.getRankPacket(infos["rang"])
+        rankPacket = rankPacket.decode("hex")
 
         time_display = app.config["TMP_AFF_TEMPS"] * 1000
         rank_display = app.config["TMP_AFF_CLASSEMENT"] * 1000
 
         print "Now displaying time"
-        timePacket=(timePacket+"ff").decode("hex")
-        display_time=int(round(time.time() * 1000))+time_display
+        timePacket = (self.scorePacket + "ff")
+        print str(timePacket)
+        timePacket = timePacket.decode("hex")
+        display_time = int(round(time.time() * 1000)) + time_display
 
-        while display_time > int(round(time.time() * 1000)): #TODO: Check if that fits the planned time
-            ser.write(timePacket)
+        while display_time > int(round(time.time() * 1000)):  #TODO: Check if that fits the planned time
+            self.ser.write(timePacket)
 
         print "Now displaying Ranking"
 
-        display_time=int(round(time.time() * 1000))+rank_display
+        display_time = int(round(time.time() * 1000)) + rank_display
         while display_time > int(round(time.time() * 1000)):
-            ser.write(rankPacket)
-        ser.setRTS(False)
-        print "Display finished"
+            self.ser.write(rankPacket)
+        self.ser.setRTS(False)
+        print "Display finished. RTS set on false."
+        self.scorePacket = ""
         return
 
-    def saveRunner(self, currentPacket):
+    def saveRunner(self):
 
-        currentPacket = currentPacket.replace("a", "0")
+        print "Now using packet : ", self.scorePacket
+        currentPacket = self.scorePacket.replace("a", "0")
+        print "Now working with packet : ", currentPacket
 
         currentPen = int(currentPacket[6:8])
-        currentTime = int(currentPacket[5]+currentPacket[2:4]+currentPacket[0:2])
-        if self.penaltyTime!=0:
-            currentTime+=self.penaltyTime
-        #if currentTime < int(app.config["TMP_CHARGE_CHRONO"])*1000:
-          #  return
-        currentNumber = int(currentPacket[21] + currentPacket[18]) + 100 * self.hundredNumberRunner
+        currentTime = int(currentPacket[5] + currentPacket[2:4] + currentPacket[0:2])
+        #if currentTime < int(app.config["TMP_CHARGE_CHRONO"])*100:
+        #  return
 
-        print "Current Number : {0} , current time : {1} , current penalties : {2}".format(str(currentNumber),
+        print "Current Number : {0} , current time : {1} , current penalties : {2}".format(str(self.currentNumber),
                                                                                            str(currentTime),
                                                                                            str(currentPen))
+        currentRunner = self.getRunner()
 
-        currentRunner=db.session.query(Participant).filter(Participant.num_depart == currentNumber
-                    and Participant.id_epreuve == app.config["CURRENT_EPREUVE_ID"])
+        if currentRunner.temps_init is None or currentRunner.temps_init is 0:
+            print "Time_init will be added"
+            currentRunner.points_init = currentPen
+            currentRunner.temps_init = currentTime
+            db.session.commit()
 
-        print db.session.query(currentRunner.exists()).all()
-        if "True" in str(db.session.query(currentRunner.exists()).all()):
-            print "Runner already in db"
-
-            if currentRunner.first().temps_init == None or currentRunner.first().temps_init == 0:
-                print "Runner has now a time_init"
-                currentRunner.first().points_init=currentPen
-                currentRunner.first().temps_init=currentTime
-                db.session.commit()
-
-            elif currentRunner.first().temps_barr == None or currentRunner.first().temps_barr == 0:
-                print "Runner has now a time_barr"
-                currentRunner.first().points_barr=currentPen
-                currentRunner.first().temps_barr=currentTime
-                db.session.commit()
-
-            else:
-                print "Runner has now a time_barr2"
-                currentRunner.first().points_barr2=currentPen
-                currentRunner.first().temps_barr2=currentTime
-                db.session.commit()
+        elif currentRunner.temps_barr is None or currentRunner.temps_barr is 0:
+            print "Time_barr will be added"
+            currentRunner.points_barr = currentPen
+            currentRunner.temps_barr = currentTime
+            db.session.commit()
 
         else:
-            print "Runner not in db, creating him/her"
-            runnerToAdd = Participant(
-                num_depart=currentNumber, points_init=currentPen, temps_init=currentTime,
-                id_epreuve=app.config["CURRENT_EPREUVE_ID"])
-            db.session.add(runnerToAdd)
+            print "Time_barr2 will be added/modified"
+            currentRunner.points_barr2 = currentPen
+            currentRunner.temps_barr2 = currentTime
             db.session.commit()
-        penaltyTime = 0
+        if not app.config["SEND_AFF"]:
+            self.isRunning = False
 
     def capture(self):
         currentPacket = ""
         currentChar = ""
-        firstSynchro=ser.read(1).encode("hex")
-        while firstSynchro != SYNCHRO:
-            firstSynchro=ser.read(1).encode("hex")
-        while currentChar != SYNCHRO:
+        firstSynchro = self.ser.read(1).encode("hex")
+        while firstSynchro != self.SYNCHRO:
+            firstSynchro = self.ser.read(1).encode("hex")
+        while currentChar != self.SYNCHRO:
             currentPacket += currentChar
-            currentChar = ser.read(1).encode("hex")
-        print currentPacket
-        if currentPacket[4] != '0':
-            print "4 : ",currentPacket[4],", 22 : ", currentPacket[22],", 19 : ", currentPacket[19]
-            self.checkPCCommand(currentPacket[4], int(currentPacket[22] + currentPacket[19]))
-        if oldPacket != currentPacket:
-            global oldPacket
-            oldPacket = currentPacket
-            if currentPacket[0] == BLANK[0]:
-                self.isRunning=True
-                self.hasDisplayed=False
-            else:
-                if self.isRunning==True:
-                    self.saveRunner(currentPacket)
-                    if self.hasDisplayed==False:
-                        self.display(int(currentPacket[21] + currentPacket[18]) + 100 * self.hundredNumberRunner, currentPacket)
-                        self.hasDisplayed=True
-                self.isRunning=False
-
+            currentChar = self.ser.read(1).encode("hex")
+        print "Packet : ", currentPacket, " acquired"
+        if currentPacket[0:2] != self.BLANK and currentPacket[0:2] != '00' and currentPacket != '1':
+            print "Saving scorePacket because ", currentPacket[0:2]
+            self.scorePacket = currentPacket
+            self.isRunning = True
+            print "Scorepacket is : ", self.scorePacket
+        if self.oldPacket != currentPacket:
+            self.oldPacket = currentPacket
+            if self.oldPacket[4] != '0' and self.ishex(self.oldPacket[4]):
+                self.checkPCCommand()
 
     def run(self):
-         while True:
+        while True:
             self.capture()
-        #ser.flush()
-        #ser.close()
-        #exit()
+
 
 
 
